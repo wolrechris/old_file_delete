15f0b0708e259032405b37cf8e13f75b3238871d
Chris <christian.drotleff@mailbox.org>
2024-11-25 02:26:43 +0100

    update entrypoint for blocking

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 echo "Starting old-file-delete at:"
 date
-echo "Running once without cron"
-python3 /usr/local/app/src/delete.py > /var/log/cron.log
-cron
-tail -f /var/log/cron.log
+python3 /usr/local/app/src/delete.py
